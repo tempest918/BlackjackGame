b9920e59c9c6e92fc51d289f800f0f23dabdfddb
Anthony Barnes <tonylbarnes@gmail.com>
2024-03-27 18:27:59 -0400

    add comments

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 
 namespace BlackjackGame
 {
+    // Card class to represent a single card
     class Card(string suit, string face, int value)
     {
         public string Suit { get; set; } = suit;
@@ -15,17 +16,20 @@ namespace BlackjackGame
         }
     }
 
+    // Deck class to represent a deck of cards
     class Deck
     {
         public List<Card> Cards { get; set; }
 
         public Deck()
         {
+            // Initialize variables and create a new list of cards
             Cards = new List<Card>();
             string[] suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
             string[] faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
             int[] values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 
+            // Add all cards to the deck
             for (int i = 0; i < suits.Length; i++)
             {
                 for (int j = 0; j < faces.Length; j++)
@@ -35,6 +39,7 @@ namespace BlackjackGame
             }
         }
 
+        // Shuffle the deck
         public void Shuffle()
         {
             Random rand = new();
@@ -45,6 +50,7 @@ namespace BlackjackGame
             }
         }
 
+        // Draw a card from the deck
         public Card Draw()
         {
             Card drawnCard = Cards[0];
@@ -52,38 +58,42 @@ namespace BlackjackGame
             return drawnCard;
         }
 
-    public void DisplayDeck()
-    {
-        Console.WriteLine("Cards in deck:");
-        foreach(Card card in Cards)
+        // Display all cards in the deck
+        public void DisplayDeck()
         {
-            card.Display();
+            Console.WriteLine("Cards in deck:");
+            foreach (Card card in Cards)
+            {
+                card.Display();
+            }
         }
-    }
     }
 
     class BlackjackGame
     {
+        // Deck property to store the deck of cards
         public Deck Deck { get; set; }
 
+        // Constructor to initialize the deck
         public BlackjackGame()
         {
             Deck = new Deck();
         }
     }
 
-        class Program 
+    class Program
     {
+        // Main method to test the Deck class
         static void Main(string[] args)
         {
             BlackjackGame game = new BlackjackGame();
 
             Console.WriteLine("Deck Order (Initial)");
-            game.Deck.DisplayDeck(); 
+            game.Deck.DisplayDeck();
 
             game.Deck.Shuffle();
             Console.WriteLine("\nDeck Order (After Shuffling)");
-            game.Deck.DisplayDeck(); 
+            game.Deck.DisplayDeck();
         }
     }
 }
